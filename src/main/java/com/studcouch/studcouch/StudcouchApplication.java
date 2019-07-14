2e69cc7331c486f563e7ade8ea87e8f1c5e4484e
Maks-Chornyi <maksym.chornyi@oa.edu.ua>
2019-07-14 22:50:23 +0300

    initial commit

---
+++ src/main/java/com/studcouch/studcouch/StudcouchApplication.java
@@ -0,0 +1,13 @@
+package com.studcouch.studcouch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudcouchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudcouchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/studcouch/studcouch/StudcouchApplicationTests.java
@@ -0,0 +1,16 @@
+package com.studcouch.studcouch;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StudcouchApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
