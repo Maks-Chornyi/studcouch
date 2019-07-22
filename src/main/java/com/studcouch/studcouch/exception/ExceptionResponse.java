8337df0c92731071b0c21c73029acf430c39b757
Maks-Chornyi <maksym.chornyi@oa.edu.ua>
2019-07-22 16:51:49 +0300

    customized default exception handler

--- src/main/java/com/studcouch/studcouch/exception/ExceptionResponse.java
+++ src/main/java/com/studcouch/studcouch/exception/ExceptionResponse.java
@@ -1,6 +1,22 @@
 package com.studcouch.studcouch.exception;
 
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
 public class ExceptionResponse {
+
+    private Date timestamp;
+    private String message;
+    private String details;
+
+    public ExceptionResponse(Date timestamp, String message, String details) {
+        super();
+        this.timestamp = timestamp;
+        this.message = message;
+        this.details = details;
+    }
 
 
 }
